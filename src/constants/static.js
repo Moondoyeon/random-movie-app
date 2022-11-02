b8c1577556423fb9172d389d84f349102426bdfe
Moondoyeon <mmmdo21@gmail.com>
2022-11-02 16:07:38 +0900

    rename:정적데이터 관심사 분리

---
+++ src/constants/static.js
@@ -0,0 +1,15 @@
+export const slotCountry = {
+  0: ["국내", "K"],
+  1: ["외국", "F"],
+};
+export const slotYear = {
+  0: "2022",
+  1: "2021",
+  2: "2020",
+  3: "2019",
+  4: "2018",
+};
+export const slotType = {
+  0: ["상업영화", "N"],
+  1: ["다양성영화", "Y"],
+};
